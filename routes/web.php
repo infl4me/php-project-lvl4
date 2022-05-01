fa29cee9885df1aee5a703c2d352c07eb7bdee94
udf <hellpl4y@gmail.com>
2022-05-02 00:36:24 +0500

    test heroku

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,5 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    $items = DB::select("SELECT * from test");
-    return view('welcome', compact('items'));
+    return view('welcome');
 });
