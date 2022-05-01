0d3d1055d5be8946023a621aabe7db5421f199c7
udf <hellpl4y@gmail.com>
2022-05-02 00:23:40 +0500

    test heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
                 <h1 class="display-3">Менеджер Задач</h1>
+                {{json_encode($items)}}
             </div>
         </div>
     </div>
