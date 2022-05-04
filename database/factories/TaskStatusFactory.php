f8299e01fcae1eb0249680c8025045a09e885ae4
udf <hellpl4y@gmail.com>
2022-05-04 20:28:07 +0500

    fix tests

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -17,7 +17,7 @@ class TaskStatusFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->randomElement(['new', 'in_progress', 'testing', 'complete']),
+            'name' => $this->faker->text(15),
         ];
     }
 }
