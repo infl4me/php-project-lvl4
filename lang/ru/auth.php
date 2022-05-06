442ffd36b87e7e62e928111a9ed4013dd984097b
udf <hellpl4y@gmail.com>
2022-05-07 02:18:47 +0500

    update

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Введите правильные имя пользователя и пароль',
+    'password' => 'Неправильный пароль',
+    'throttle' => 'Исчерпано количество попыток. Попробуйте снова через :seconds секунд',
 
 ];
