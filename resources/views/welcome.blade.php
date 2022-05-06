ab2f456f4d5a48c38c63743608c24894b37be116
udf <hellpl4y@gmail.com>
2022-05-06 23:51:15 +0500

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-lg">
+    <div class="container py-4">
         <div class="p-5 mb-4 bg-light border rounded-3">
             <div class="container-fluid py-5">
-                <h1 class="text-center display-5 fw-bold">Менеджер задач</h1>
+                <h1 class="display-5 fw-bold">Привет от Хекслета!</h1>
+                <p class="col-md-8 fs-4">Практические курсы по программированию</p>
+                <button href="https://hexlet.io" class="btn btn-primary btn-lg" type="button">Узнать больше</button>
             </div>
         </div>
     </div>
