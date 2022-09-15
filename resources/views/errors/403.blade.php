ce6b1d37f25706217f879795b9f1aadcb31d9bb0
udf <hellpl4y@gmail.com>
2022-09-15 05:34:14 +0500

    add custom 403 and 404 pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('content')
+  <div class="container-lg">
+    <div class="row">
+      <h3 class="text-center">
+        403
+      </h3>
+      <h3 class="text-center">
+        Доступ запрещен
+      </h3>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('content')
+  <div class="container-lg">
+    <div class="row">
+      <h3 class="text-center">
+        404
+      </h3>
+      <h3 class="text-center">
+        Страница не найдена
+      </h3>
+    </div>
+  </div>
+@endsection
